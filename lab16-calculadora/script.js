ac09961589189f38aa986779dd2575b92eb9e960
rafaelmnf <rafaelnfilho@gmail.com>
2024-09-02 14:42:50 -0300

    troca de pasta

---
+++ lab16-calculadora/script.js
@@ -0,0 +1,40 @@
+let operacaoAtual = ''; 
+let resultado = 0;    
+let operador = '';     
+let operacao = ''; 
+
+
+function addNum(numero) {
+    operacaoAtual += numero;
+    atualizarTela(operacaoAtual);
+}
+
+function addOperacao(operacao) {
+    if (operacaoAtual !== '') {
+        operador = operacao;
+        operacaoAtual += operacao;
+        atualizarTela(operacaoAtual);
+    }
+}
+
+function calcular() {
+    try {
+        resultado = eval(operacaoAtual);
+        atualizarTela(resultado);
+        operacaoAtual = resultado.toString();
+    } catch (e) {
+        atualizarTela('erro');
+        operacaoAtual = '';
+    }
+}
+
+function limpar() {
+    operacaoAtual = '';
+    operador = '';
+    resultado = 0;
+    atualizarTela(0);
+}
+
+function atualizarTela(valor) {
+    document.getElementById('resultado').innerHTML = valor;
+}
